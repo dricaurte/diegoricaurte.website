0159f0c4222c8d58906764c9eb8bf70ebccc700e
dricaurte <daricaurte@gmail.com>
2016-12-07 17:33:08 -0500

    Real Picture Update

--- index.php
+++ index.php
@@ -8,24 +8,19 @@
 	<div class="loader">
 	</div>
 	<!--Loader-->
-	<section id= "welcome">
-		<div class= "container-fluid splashimage">
-			<!-- <div class="overlay"></div>	 -->
+	<section class="splashimage" id="welcome">
+		<div class="container intromessage">
 			<div class= "row">
-				<div class="col-xs-12 intromessage">
-					<div class= "row">
-						<div class="col-xs-12">
-							<div>
-								<img src="images/signature.svg" class="indexsignature centered img-responsive">
-							</div>
-						</div>
+				<div class="col-xs-12">
+					<div>
+						<img src="images/signature.svg" class="indexsignature centered img-responsive">
 					</div>
-					<div class= "row">
-						<div class="col-xs-12">
-							<div>
-								<a href="#resume" class="borderBtn scrollto">MORE ABOUT ME</a>
-							</div>
-						</div>
+				</div>
+			</div>
+			<div class= "row">
+				<div class="col-xs-12">
+					<div>
+						<a href="#resume" class="borderBtn scrollto">MORE ABOUT ME</a>
 					</div>
 				</div>
 			</div>
@@ -70,7 +65,7 @@
 				</div>
 				<div class="heading">
 					<h1>DIEGO RICAURTE</h1>
-					<h3>Graphic and Digital Designer</h3>
+					<h3>Graphic and Web Designer</h3>
 					<a href="files/resume.pdf" class="borderBtn">DOWNLOAD RESUME</a>
 				</div>
 			</div>
@@ -80,14 +75,14 @@
 	<!--Services Section-->
     <section class="services section" id="about">
 		<div class="container">
-			<div class= "row col-xs-12 centered">
+			<div class= "row">
 				<div class="divider"></div>
 				<div class="heading">
 					<h2>WHO I AM AND WHAT I DO</h2>
 					<p>I create logical solutions for my clients </p>
 				</div>
-				<div>
-					<p>My name is Diego Ricaurte, and I am a graphic and digital designer from Ellicott City, Maryland. I am passionate about design that 
+				<div class="blurb">
+					<p>My name is Diego Ricaurte, and I am a graphic and web designer from Ellicott City, Maryland. I am passionate about design that 
 					is not only pleasing to the eye, but also intuitive to the user. I believe effective design can shed light on solutions that previously
 					seemed impossible. I enjoy learning new skills and have a steep learning curve. I love working with others to create the best possible product, and I take pride 
 					in my ability to adapt to any setting with any team. With a little help from a cup of coffee, I believe a positive attitude and rigorous work ethic can
@@ -97,7 +92,7 @@
 				<div class="services-inner hidethis">
 					<div class="service col-xs-12 col-sm-6 col-md-3 col-lg-3">
 						<p class="icon"><i class="screen"></i></p>
-						<h3>DIGITAL DESIGN</h3>
+						<h3>WEB DESIGN</h3>
 					</div> 
 					<div class="service col-xs-12 col-sm-6 col-md-3 col-lg-3">
 						<p class="icon"><i class="pen"></i></p>
@@ -187,7 +182,7 @@
 				<div class="heading">
 					<h2>PORTFOLIO</h2>
 				</div>
-	            <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 index thumb">
+	            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 index thumb">
 	                <a class="index thumbnail" href="the-key-elements-spread.php">
 	                    <img class="img-responsive" src="images/portfolio-thumbs/the-key-elements-thumb.png" alt="">
 		                <div class = "index caption">
@@ -195,7 +190,7 @@
 		         		</div>
 	                </a>
 	            </div>
-	            <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 index thumb">
+	            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 index thumb">
 	                <a class="index thumbnail" href="atom-family-spread.php">
 	                    <img class="img-responsive" src="images/portfolio-thumbs/atom-family-thumb.png" alt="">
 	                	<div class = "index caption">
@@ -203,7 +198,7 @@
 	         			</div>
 	                </a>
 	            </div>
-	            <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 index thumb">
+	            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 index thumb">
 	                <a class="index thumbnail" href="agv-sport-spread.php">
 	                    <img class="img-responsive" src="images/portfolio-thumbs/agv-sport-thumb.png" alt="">
 	                	<div class = "index caption">
@@ -211,7 +206,7 @@
 	         			</div>
 	                </a>
 	            </div>
-	            <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 index thumb">
+	            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 index thumb">
 	                <a class="index thumbnail" href="dr-design-spread.php">
 	                    <img class="img-responsive" src="images/portfolio-thumbs/dr-design-thumb.png" alt="">
 		                <div class = "index caption">
@@ -219,7 +214,7 @@
 		         		</div>
 	                </a>
 	            </div>
-	            <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 index thumb">
+   				<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 index thumb">
 	                <a class="index thumbnail" href="tournament-of-heroes-spread.php">
 	                    <img class="img-responsive" src="images/portfolio-thumbs/tournament-of-heroes-thumb.png"  alt="">
 		                <div class = "index caption">
@@ -227,7 +222,7 @@
 		         		</div>
 	                </a>
 	            </div>
-	            <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 index thumb">
+	            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 index thumb">
 	                <a class="index thumbnail" href="hard-rock-cafe-baltimore-spread.php">
 	                    <img class="img-responsive" src="images/portfolio-thumbs/hard-rock-cafe-thumb.png" alt="">
 		                <div class = "index caption">
@@ -235,7 +230,7 @@
 		         		</div>
 	                </a>
 	            </div>
-	            <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 index thumb">
+	            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 index thumb">
 	                <a class="index thumbnail" href="wake-will-spread.php">
 	                    <img class="img-responsive" src="images/portfolio-thumbs/wake-will-thumb.png" alt="">
 		                <div class = "index caption">
@@ -243,7 +238,7 @@
 		         		</div>
 	                </a>
 	            </div>
-	            <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 index thumb">
+	            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 index thumb">
 	                <a class="index thumbnail" href="arms-race-spread.php">
 	                    <img class="img-responsive" src="images/portfolio-thumbs/arms-race-thumb.png" alt="">
 	                <div class = "index caption">
@@ -251,7 +246,7 @@
 	         		</div>
 	                </a>
 	            </div>
-	            <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 index thumb">
+	            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 index thumb">
 	                <a class="index thumbnail" href="gtech-designs-spread.php">
 	                    <img class="img-responsive" src="images/portfolio-thumbs/gtech-thumb.png" alt="">
 	                <div class = "index caption">
@@ -259,7 +254,7 @@
 	         		</div>
 	                </a>
 	            </div>
-	            <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 index thumb">
+	             <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 index thumb">
 	                <a class="index thumbnail" href="photography-spread.php">
 	                    <img class="img-responsive" src="images/portfolio-thumbs/photography-thumb.png" alt="">
 	                <div class = "index caption">
