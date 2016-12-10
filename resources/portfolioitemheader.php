4eb02138bfeeab5d048338691a8054b336b1b6c9
dricaurte <daricaurte@gmail.com>
2016-12-10 15:00:54 -0500

    New Website fixes
    Removed minified codes
    Fixed footer pixelation
    Fixed splash signature

--- resources/portfolioitemheader.php
+++ resources/portfolioitemheader.php
@@ -3,7 +3,7 @@
     <div class="col-xs-12">
       <div class="col-xs-3 portfolioitemsignaturecontainer centered">
         <a href="index.php">
-            <img class="portfolioitemsignature centered" src="images/portfolio-item-signature.svg" alt="">
+            <img class="portfolioitemsignature centered" src="images/badge.svg" alt="">
         </a>
       </div>
     </div>
